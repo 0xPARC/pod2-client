15ffb345747be45ebbb736682c3239cc84fd8377
Rob Knight <mail@robknight.org.uk>
2025-07-16 19:30:53 -0700

    Delete server and playground-client components

--- server/migrations/02-private_keys/up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE private_keys (
-    private_key TEXT PRIMARY KEY,
-    key_type TEXT NOT NULL, /* e.g. Mock, Plonky2 */
-    public_key TEXT NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-);
